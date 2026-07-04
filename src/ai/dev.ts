135c9905de7c65897a656f220237edb93edc86bd
speditionhueber-lang <speditionhueber@icloud.com>
2026-07-04 02:43:49 +0000

    Remove Genkit and fix build

--- src/ai/dev.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-// Flows will be imported for their side effects in this file.
-import './flows/calculate-distance-flow';
-import './flows/transcribe-audio';
-import './flows/summarize-customer-flow';
-import './flows/extract-invoice-data-flow';
-import './flows/extract-customer-data';
-import './flows/summarize-conversation';
