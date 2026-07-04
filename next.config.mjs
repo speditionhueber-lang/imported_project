558df14502b4f98d57245d213b3bfa10d06183c6
speditionhueber-lang <speditionhueber@icloud.com>
2026-07-04 02:36:41 +0200

    Add optimizeFonts option to Next.js config
    Added optimizeFonts option to nextConfig.

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve.fallback = {
-                ...config.resolve.fallback,
-                net: false,
-                tls: false,
-                fs: false,
-                child_process: false,
-            };
-        }
-        config.externals.push('gaxios');
-        config.externals.push('googleapis');
+  reactStrictMode: true,
 
+  optimizeFonts: false,
 
-        return config;
-    },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        net: false,
+        tls: false,
+        fs: false,
+        child_process: false,
+      };
+    }
+
+    config.externals.push('gaxios');
+    config.externals.push('googleapis');
+
+    return config;
+  },
 };
 
 export default nextConfig;
